9a74ac7a13d3521971b2b77414776e13cf181c27
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-04-15 12:17:50 -0300

    feat: Creating initial structure for interest card

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { InputComponent } from "../../components/input/input.component";
 import { ButtonComponent } from "../../components/button/button.component";
 import { CheckboxComponent } from "../../components/checkbox/checkbox.component";
+import { InterestCardComponent } from "../../components/interest-card/interest-card.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [InputComponent, ButtonComponent, CheckboxComponent],
+  imports: [InputComponent, ButtonComponent, CheckboxComponent, InterestCardComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
