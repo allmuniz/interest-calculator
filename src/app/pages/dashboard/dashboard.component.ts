a4134b7ede4fce0e234fb2760136ffbba57dbff8
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-04-15 10:08:18 -0300

    feat: Creating initial structure for button

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { InputComponent } from "../../components/input/input.component";
+import { ButtonComponent } from "../../components/button/button.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [InputComponent],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
