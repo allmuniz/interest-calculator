8050e5bb824eede5c0d477fefd97e982ccd0239a
Allan Muniz <146678270+allmuniz@users.noreply.github.com>
2025-04-15 11:02:40 -0300

    feat: Creating initial structure for checkbox

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InputComponent } from "../../components/input/input.component";
 import { ButtonComponent } from "../../components/button/button.component";
+import { CheckboxComponent } from "../../components/checkbox/checkbox.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [InputComponent, ButtonComponent],
+  imports: [InputComponent, ButtonComponent, CheckboxComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
